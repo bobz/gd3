7157d619fa903dbd9b4fafd4ac510740f8c8aa60
bobz <bobz@bobz.in>
2011-09-11 23:22:20 +0000

    rails31 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gd3::Application.config.secret_token = '4934d37d5c1d8e2a4a09f8e2fc02f486191d68f58e5ec46398df62799a780672909e2fa07349b57ebf402f81336aae406e5edd597b86d01b219b7f23c56628eb'
